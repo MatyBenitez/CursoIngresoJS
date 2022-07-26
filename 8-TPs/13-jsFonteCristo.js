76bae2b1c15e4f8c4ccd5bc8b3dd98c125965c7d
MatyBenitez <matyuz1412@gmail.com>
2022-07-26 16:25:53 -0300

    clase

--- 8-TPs/13-jsFonteCristo.js
+++ 8-TPs/13-jsFonteCristo.js
@@ -1,13 +1,56 @@
-/*El departamento de NUMEROS ESPECIALES  del instituto matemático  FonteCristo  nos está pidiendo una aplicación que verifique las distintas cualidades de los números.
-
+/*
+Benitez Matias
+DIV X
+TP 13
+El departamento de NUMEROS ESPECIALES  del instituto matemático  FonteCristo  nos está pidiendo una aplicación que verifique las distintas cualidades de los números.
 13.	Para cada una de estas acciones  debemos realizar la lógica para verificar las cualidades pedidas:
-a.	Se pedirán un número positivo y se mostrará la cantidad de números pares desde el número ingresado hasta el cero.
-b.	Se pedirán un número positivo y se mostrará la cantidad de números pares desde el número ingresado hasta el cero.
-c.	Se pedirán un número positivo y se mostrará la cantidad de números divisibles de este número que se encuentran desde el 1 al 100.
-d.	Se pedirán un número positivo y se mostrará si el número es un número primo o no.
-e.	Se pedirán un número positivo y se mostrará la cantidad de números Primos desde el número ingresado hasta el cero.
 */
-function ComenzarIngreso () 
+
+// a.	Se pedirán un número positivo y se mostrará la cantidad de números pares desde el número ingresado hasta el cero.
+function NumerosPares () 
 {
- 	
+ 	let numero;
+    let contPares;
+    let i; //variable de control para controlar el for;
+    
+    contPares = 0;
+    numero = document.getElementById("txtIdNumero").value;
+    numero = parseFloat(numero);
+
+    for(i = 1; i <= numero; i++)
+    {
+        if(i % 2 == 0)
+        {
+            contPares++;
+            console.log("<br> los numeros pares son: " + i);
+        }
+    }
+    document.write("la cantidad de numeros pares es: " + contPares);
+}
+
+// b.	Se pedirán un número positivo y se mostrará la cantidad de números impares desde el número ingresado hasta el cero.
+function NumerosImpares ()
+{
+    
+}
+
+// c.	Se pedirán un número positivo y se mostrará la cantidad de números divisibles de este número que se encuentran desde el 1 al 100.
+function NumerosDivisibles ()
+{
+    let numero;
+    let divCant;
+    let i;
+
+}
+
+// d.	Se pedirán un número positivo y se mostrará si el número es un número primo o no.
+function VerificarPrimo()
+{
+
+}
+
+// e.	Se pedirán un número positivo y se mostrará la cantidad de números Primos desde el número ingresado hasta el cero.
+function NumerosPrimos()
+{
+
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,38 @@
+/*
+Enunciado:
+Bienvenidos.
+Realizar el algoritmo que permita el ingreso por prompt de las notas (validar entre 0 y 10) , el sexo (validar el sexo “f” o “m”) de 5 alumnos, informar por alert:
+a) El promedio de las notas totales.
+b) La nota más baja y el sexo de esa persona.
+c) La cantidad de varones que su nota haya sido mayor o igual a 6.
+*/
 function mostrar()
 {
+    let nota;
+    let sexo;
+    let i;
+    let promedio;
+    let notaTotal;
+    for(i = 0; i < 5; i++)
+    {
+        do
+        {
+            nota = prompt("ingrese su nota");
+            nota = parseFloat(nota);
+        }while(isNaN(nota) || nota < 0 || nota > 10);
 
+        do
+        {
+            sexo = prompt("ingrese su sexo. f/m");
+            sexo = sexo.toLowerCase();
+        }while(sexo != 'f' && sexo != 'm');
+
+        notaTotal = notaTotal + nota;
+        
+        while(nota < 0)
+        {
+            nota = 
+        }
+    }
+    promedio = nota / i;
 }
