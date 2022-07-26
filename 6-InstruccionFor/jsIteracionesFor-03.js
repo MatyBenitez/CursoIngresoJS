a6dc6bb6f9e6fc578849be8ec70120ea6ba0f875
MatyBenitez <matyuz1412@gmail.com>
2022-07-26 00:13:21 -0300

    ult

--- 6-InstruccionFor/jsIteracionesFor-03.js
+++ 6-InstruccionFor/jsIteracionesFor-03.js
@@ -6,9 +6,15 @@ function mostrar()
 {
 
 	let repetciones;
+	let cont;
 	
 	repetciones = prompt("ingrese el número de veces que quiere que se repita");
-	alert("ok");
+
+	for()
+	{
+
+	}
+	
 
 
 }//FIN DE LA FUNCIÓN
