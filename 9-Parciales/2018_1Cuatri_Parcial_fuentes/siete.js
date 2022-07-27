699c5301459db48a469625fc8b030cb6b91d9575
MatyBenitez <matyuz1412@gmail.com>
2022-07-26 22:32:12 -0300

    ult

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -87,24 +87,34 @@ function mostrar()
     let pesoKilo;
     let precioKilo;
     let respuesta;
-    let flag1;
+    let flag1; //para descuento
     let descuento;
     let totalBruto;
-    let total;
+    let totalDescuento;
     let tipo;
     let pesoTotal;
     let precioTotal;
+    let flag2; //alimento mas caro
+    let tipoCaro;
+    let precioCaro;
+    let promedioKilo;
 
     respuesta = 's';
     flag1 = 0;
     descuento = 0;
+    flag2 = 0;
+    pesoKilo = 0;
+    precioKilo = 0;
+    pesoTotal = 0;
+    precioTotal = 0;
+    promedioKilo = 0;
 
     while(respuesta == 's')
     {
         pesoKilo = prompt("ingrese el peso en Kg");
         pesoKilo = parseFloat(pesoKilo);
 
-        while((isNaN(peso)) || peso < 10 || peso > 1000)
+        while((isNaN(pesoKilo)) || pesoKilo < 10 || pesoKilo > 1000)
         {
             pesoKilo = prompt("vuelva a ingresar el peso en Kg entre 10 y 1000 Kg");
             pesoKilo = parseFloat(pesoKilo);
@@ -121,76 +131,62 @@ function mostrar()
             precioTotal = precioTotal + precioKilo;
         }
 
+        totalBruto = pesoTotal * precioKilo;
+
         tipo = prompt("ingrese un ingrediente: 'v' 'a' 'm' vegetal,animal o mezcla )");
-        tipo = tipo.toLowerCase;
-        while((!isNaN(tipo)) || tipo == 'v' || tipo == 'a' || tipo == 'm')
+        //tipo = tipo.toLowerCase;
+
+        while(tipo == 'v' && tipo == 'a' && tipo == 'm')
         {
             tipo = prompt("Error, ingrese bien un ingrediente: 'v' 'a' 'm' vegetal,animal o mezcla )");
             tipo = tipo.toLowerCase;
         }
-
-        totalBruto = pesoKilo * precioKilo;
-
-        if(pesoKilo > 100)
+        
+        if(flag1 == 0)
         {
-            descuento = 0.15;
-        }
-        else
-        {
-            if(pesoKilo > 300)
+            if(pesoKilo > 100)
             {
-                descuento = 0.25;
+              descuento = 0.15;
+              flag1 = 1;
+            }
+            else
+            {
+                if(pesoKilo > 300)
+                {
+                    descuento = 0.25;
+                    flag1 = 1;
+                }
             }
         }
 
-
-
-
-
-
-
-
+        if(flag2 == 0)
+        {
+            tipoCaro = tipo;
+            precioCaro = precioKilo;
+            flag2 = 1;
+        }
+        else
+        {
+            if(precioKilo > precioCaro)
+            {
+                tipoCaro = tipo;
+                precioCaro = precioKilo;
+            }
+        }
 
         respuesta = prompt("¿Quiere seguir ingresando datos? s/n");
-       respuesta = respuesta.toLowerCase();
-
+        respuesta = respuesta.toLowerCase();
     }
+    console.log(tipoCaro);
+    promedioKilo = precioTotal / pesoTotal;
 
+    document.write("el imposte total bruto a pagar es de: " + totalBruto);
+    document.write("<br>El alimento mas caro es: " + tipoCaro + "con un precio de: " + precioCaro + " por Kg");
+    document.write("<br>El promedio total por Kg es de: " + promedioKilo);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if(flag1 = 1)
+    {
+        totalDescuento = totalBruto * descuento;
+        document.write("<br>Su importe total con descuento es de: " + totalDescuento);
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
